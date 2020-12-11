dc0781232a8897aaf842992ed473c2181a14fa0a
Steven Acevedo <astevenaa@gmail.com>
2020-12-11 12:07:51 -0700

    fixed db connection

---
+++ client/build/precache-manifest.3ca53d8187aef182c6578b263a9781d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371ade46e4a2ebe59117e84fe3b87ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a944ed40acfd3729e85",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "ec1b0a6c0157cdc31bbb",
+    "url": "/static/js/2.516cc31c.chunk.js"
+  },
+  {
+    "revision": "07d28acefe4f478bb7a9400a0790acb9",
+    "url": "/static/js/2.516cc31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a944ed40acfd3729e85",
+    "url": "/static/js/main.24b96341.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
