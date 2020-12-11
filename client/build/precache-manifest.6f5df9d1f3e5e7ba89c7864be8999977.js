f436f05c50977b8a82c21ef33846ab3bdf9e656c
Steven Acevedo <astevenaa@gmail.com>
2020-12-11 14:00:00 -0700

    heroku open

---
+++ client/build/precache-manifest.6f5df9d1f3e5e7ba89c7864be8999977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e2926692ccf103c995f8638eab2396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edc061ee01d2424c1ae",
+    "url": "/static/css/main.7b6c8808.chunk.css"
+  },
+  {
+    "revision": "ec1b0a6c0157cdc31bbb",
+    "url": "/static/js/2.516cc31c.chunk.js"
+  },
+  {
+    "revision": "07d28acefe4f478bb7a9400a0790acb9",
+    "url": "/static/js/2.516cc31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2edc061ee01d2424c1ae",
+    "url": "/static/js/main.82bac81e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
