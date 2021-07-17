a51d3ddffc87a397c31fd705a694f4a0d28dc86e
astevenaa <astevenaa@gmail.com>
2021-07-17 14:49:30 -0700

    deploy

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
-
 // Connect Database
 connectDB();
-
 // Init Middleware
 app.use(express.json({extended: false}));
 
@@ -28,4 +26,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
